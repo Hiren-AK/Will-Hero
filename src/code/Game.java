bcfa186d3835162f70fbcc4187ac153926702ce2
hiren066 <hiren20066@iiitd.ac.in>
2021-12-25 11:34:37 +0530

    Made Game class and started game from there

--- src/code/Game.java
+++ src/code/Game.java
@@ -1,8 +1,34 @@
 package code;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Game {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private FXMLLoader loader;
 
-    public Game(){
+    public Game(Stage stage){
+        this.stage = stage;
+    }
 
+
+    public void startGame() throws IOException {
+        loader = new FXMLLoader(getClass().getResource("Start.fxml"));
+        root = loader.load();
+        stage.setTitle("Will Hero");
+        Image icon = new Image("/assets/logo.png");
+        Scene scene = new Scene(root);
+        stage.setResizable(false);
+        scene.getStylesheets().add(getClass().getResource("/assets/StyleSheet.css").toExternalForm());
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/code/Main.java
+++ src/code/Main.java
@@ -11,21 +11,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
-        Parent root = loader.load();
-        Controller controller = loader.getController();
-        primaryStage.setTitle("Will Hero");
-        Image icon = new Image("/assets/logo.png");
-        Scene scene = new Scene(root);
-        primaryStage.setResizable(false);
-        scene.getStylesheets().add(getClass().getResource("/assets/StyleSheet.css").toExternalForm());
-        primaryStage.getIcons().add(icon);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Game game = new Game(primaryStage);
+        game.startGame();
     }
 
     public static void main(String[] args) {
-        Game game = new Game();
         launch(args);
     }
 }
