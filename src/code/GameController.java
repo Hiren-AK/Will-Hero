3fad65cc6378b7a0027b5a0232f503232a0155cb
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2021-12-26 08:00:08 +0530

    Created Game controller and implemented game FXML

--- src/code/GameController.java
+++ src/code/GameController.java
@@ -1,5 +1,6 @@
 package code;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,15 +10,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class StartController implements Initializable {
-
+public class GameController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -26,7 +28,10 @@ public class StartController implements Initializable {
     private HighScore score = new HighScore();
 
     @FXML
-    private Label startPageHighScore;
+    private Label gameHighScore;
+
+    @FXML
+    private ImageView background;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -47,23 +52,20 @@ public class StartController implements Initializable {
             System.out.println("ClassNotFoundException" + " is caught");
             score.setHighScore(-1);
         }
-        startPageHighScore.setText(" " + score.getHighScore());
+        gameHighScore.setText(" " + score.getHighScore());
+
+        TranslateTransition backgroundAnimation = new TranslateTransition();
+        backgroundAnimation.setNode(background);
+        backgroundAnimation.setCycleCount(TranslateTransition.INDEFINITE);
+        backgroundAnimation.setDuration(Duration.millis(50000));
+        backgroundAnimation.setByX(-200);
+        backgroundAnimation.play();
     }
 
-    public void resumeSavedGame(ActionEvent event)throws IOException {
-        loader = new FXMLLoader(getClass().getResource("Resume.fxml"));
+    public void returnHomeFromPlayGame(ActionEvent event) throws IOException{
+        loader = new FXMLLoader(getClass().getResource("Start.fxml"));
         root = loader.load();
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/assets/StyleSheet.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void playGame(ActionEvent event)throws IOException {
-        loader = new FXMLLoader(getClass().getResource("Game.fxml"));
-        root = loader.load();
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/assets/StyleSheet.css").toExternalForm());
         stage.setScene(scene);
