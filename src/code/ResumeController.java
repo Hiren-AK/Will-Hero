9222d60d239ff28b2ec0ba17908e8183f3268ff2
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2021-12-26 18:57:58 +0530

    Added Resume and Save labels

--- src/code/ResumeController.java
+++ src/code/ResumeController.java
@@ -1,19 +1,91 @@
 package code;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import java.io.IOException;
 
-public class ResumeController {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ResumeController implements Initializable {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
     private FXMLLoader loader;
+    private Game game1;
+    private Game game2;
+    private Game game3;
+
+    @FXML
+    private Label loadSlot1;
+    @FXML
+    private Label loadSlot2;
+    @FXML
+    private Label loadSlot3;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            FileInputStream file = new FileInputStream("serial/SerializedGame1.txt");
+            ObjectInputStream in = new ObjectInputStream(file);
+            game1 = (Game)in.readObject();
+            in.close();
+            file.close();
+            loadSlot1.setText("" + game1.getScore());
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+        }
+
+        catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException is caught");
+        }
+
+        try {
+            FileInputStream file = new FileInputStream("serial/SerializedGame2.txt");
+            ObjectInputStream in = new ObjectInputStream(file);
+            game2 = (Game)in.readObject();
+            in.close();
+            file.close();
+            loadSlot2.setText("" + game2.getScore());
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+        }
+
+        catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException is caught");
+        }
+
+        try {
+            FileInputStream file = new FileInputStream("serial/SerializedGame3.txt");
+            ObjectInputStream in = new ObjectInputStream(file);
+            game3 = (Game)in.readObject();
+            in.close();
+            file.close();
+            loadSlot3.setText("" + game3.getScore());
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+        }
+
+        catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException is caught");
+        }
+    }
 
     public void returnHomeFromResumeSavedGames(ActionEvent event) throws IOException{
         loader = new FXMLLoader(getClass().getResource("Start.fxml"));
