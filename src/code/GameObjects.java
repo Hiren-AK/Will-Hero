7d4c5f5776f74ff54c4fefffb0bc90364bf2ea7d
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2022-01-02 15:38:09 +0530

    Added gameObjects class

---
+++ src/code/GameObjects.java
@@ -0,0 +1,10 @@
+package code;
+
+import javafx.scene.shape.Rectangle;
+
+public class GameObjects {
+    private Rectangle gameRectangle;
+    public GameObjects(Rectangle _gameRectangle){
+        gameRectangle = _gameRectangle;
+    }
+}
